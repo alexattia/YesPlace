6f31735d93bfa67fae4c3b3d9985acdbb08cf156
Alexandre Attia <alexandre.attia@wanadoo.fr>
2016-03-14 15:20:04 +0100

    Organizing code

--- js/app.js
+++ js/app.js
@@ -45,52 +45,20 @@ directive('myMap', function() {
                 infoWindow = new google.maps.InfoWindow(infoWindowOptions);
                 infoWindow.open(map, marker);
             });
+            
         }
 
         //function searchMarker(name){}
-        
+        function returnMarker(){
+           window.alert(markers[0].title);
+        }
         // show the map and place some markers
         initMap();
         
-        setMarker(map, new google.maps.LatLng(48.852179, 2.335257), 'Da Pietro', '<b>Da Pietro </b>: Pizza simple','images/icons/pizza2.png');
-        setMarker(map, new google.maps.LatLng(48.851535, 2.338173), 'Marco Polo', '<b>Marco Polo </b>: Pâtes à la truffe','images/icons/pasta2.png');
-        setMarker(map, new google.maps.LatLng(48.850292, 2.330776), 'Pizza Chic', '<b>Pizza Chic </b>: resto sympa','images/icons/pizza2.png');
-        setMarker(map, new google.maps.LatLng(48.852886, 2.331436), 'Bocca della Verita', '<b>Bocca della Verita</b> : Bonnes pâtes','images/icons/pasta2.png');
-        setMarker(map, new google.maps.LatLng(48.842103, 2.335142), 'Coffee Club', '<b>Coffee Club</b> : américain classique','images/icons/hamburger.png');
-        setMarker(map, new google.maps.LatLng(48.847043, 2.325963), 'Colorova', '<b>Colorova </b>: Pâtisserie, Brunch ou Gouter très sympa','images/icons/cookies.png');
-        setMarker(map, new google.maps.LatLng(48.854928, 2.341023), 'Ze Kitchen Gallery', '<b>Ze Kitchen Gallery</b> : mediterranéen, 1 etoile Michelin','images/icons/tuxedo.png');
-        setMarker(map, new google.maps.LatLng(48.856560, 2.326689), 'Gaya', '<b>Gaya </b>: 1 etoile Michelin','images/icons/tuxedo.png');
-        setMarker(map, new google.maps.LatLng(48.854450, 2.337923), 'Alcazar', '<b>Alcazar </b>: cool et assez bon','images/icons/tuxedo.png');
-        setMarker(map, new google.maps.LatLng(48.854273, 2.330790), 'Ralph', '<b>Le Ralph\'s</b>: américain chic','images/icons/hamburger.png');
-        setMarker(map, new google.maps.LatLng(48.847343, 2.318231), 'Marco Polo', '<b>Patisserie Cyril Lignac </b>: à emporter','images/icons/cookies.png');
-        setMarker(map, new google.maps.LatLng(48.853048, 2.338647), 'Pizza Chic', '<b>Eclair de Genie</b>: à emporter','images/icons/cookies.png');
-        setMarker(map, new google.maps.LatLng(48.852098, 2.346457), 'Bocca della Verita', '<b>Odette</b> : choux à la crème','images/icons/dounoughts.png');
-        setMarker(map, new google.maps.LatLng(48.849555, 2.349307), 'Coffee Club', '<b>Aux Merveilleux de Fred</b> : classique','images/icons/dounoughts.png');
-        setMarker(map, new google.maps.LatLng(48.840031, 2.335252), 'Coffee Club', '<b>Il Forno di Napoli</b> : très bonne pizza, à emporter','images/icons/pizza2.png');
-        setMarker(map, new google.maps.LatLng(48.852671, 2.331512), 'Colorova', '<b>Blueberry </b>: maki bar','images/icons/sushi-2.png');
-        setMarker(map, new google.maps.LatLng(48.853151, 2.337829), 'Ze Kitchen Gallery', '<b>Ippudo</b> : spécialiste du ramen','images/icons/sushi-3.png');
-        setMarker(map, new google.maps.LatLng(48.852228, 2.329959), 'Gaya', '<b>Bar de la Croix Rouge </b>: Toasts, Terrasse','images/icons/sun.png');
-        setMarker(map, new google.maps.LatLng(48.858175, 2.356220), 'Alcazar', '<b>Benedict </b>: brunch, souvent la queue, oeufs pochés','images/icons/egg.png');
-        setMarker(map, new google.maps.LatLng(48.857122, 2.358887), 'Colorova', '<b>Miznon </b>: cacher, israelien, fermé samedi','images/icons/meat.png');
-        setMarker(map, new google.maps.LatLng(48.854234, 2.350212), 'Colorova', '<b>Bertie’s CupCakery  </b>: meilleur cupcake Paris','images/icons/dounoughts.png');
-        setMarker(map, new google.maps.LatLng(48.862378, 2.363778), 'Colorova', '<b>Popelini </b>: choux à la crème','images/icons/cookies.png');
-        setMarker(map, new google.maps.LatLng(48.842632, 2.328002), 'Colorova', '<b>Café Atelier </b>: bistro, brunch, bon','images/icons/brunch.png'); 
-        setMarker(map, new google.maps.LatLng(48.844593, 2.331951), 'Bocca della Verita', '<b>Jean Paul Hévin</b> : Chocolatier, Macaron, Cheesecake','images/icons/cookies.png');
-        setMarker(map, new google.maps.LatLng(48.853903, 2.332277), 'Coffee Club', '<b>Café Pouchkine </b> : salon de thé franco-russe','images/icons/cookies.png');
-        setMarker(map, new google.maps.LatLng(48.848880, 2.334119), 'Coffee Club', '<b>Angelina</b> : peu de couvert, dans le Luxembourg','images/icons/tea.png');
-        setMarker(map, new google.maps.LatLng(48.844716, 2.332012), 'Colorova', '<b>Amorino </b>: glaces','images/icons/ice-cream.png');
-        setMarker(map, new google.maps.LatLng(48.854374, 2.333503), 'Ze Kitchen Gallery', '<b>La Société</b> : costes, belle terrasse, cher','images/icons/tuxedo.png');
-        setMarker(map, new google.maps.LatLng(48.865317, 2.331279), 'Gaya', '<b>La Corte</b>: Italien, très bon, à reserver','images/icons/pasta2.png');
-        setMarker(map, new google.maps.LatLng(48.888272, 2.362532), 'Alcazar', '<b>Bob’s Bake Shop </b>: tartes, bagels, toast, terrasse','images/icons/hot-dog.png');
-        setMarker(map, new google.maps.LatLng(48.867657, 2.331341), 'Colorova', '<b>Little Georgette</b>: tapas francais','images/icons/hot-dog.png');
-        setMarker(map, new google.maps.LatLng(48.866089, 2.345371), 'Colorova', '<b>La Cevicheria</b>: spécialiste des ceviche','images/icons/sushi.png');
-        setMarker(map, new google.maps.LatLng(48.872299, 2.365012), 'Colorova', '<b>Siseng </b>: hamburger blanc, asiatique','images/icons/hamburger2.png');
-        setMarker(map, new google.maps.LatLng(48.864677, 2.333851), 'Colorova', '<b>Sanukiya</b>: spécialiste du Udon)','images/icons/sushi.png');
-        setMarker(map, new google.maps.LatLng(48.877078, 2.340834), 'Colorova', '<b>Caillebotte</b>: Bistrot branché, beau décor','images/icons/vine-glass.png');
-        setMarker(map, new google.maps.LatLng(48.879298, 2.334576), 'Colorova', '<b>Les Canailles</b>: Bistrot gastro, pas très cher','images/icons/vine-glass.png');
-        setMarker(map, new google.maps.LatLng(48.858413, 2.302294), 'Colorova', '<b>Fables de la Fontaine</b>: restaurant gastro, pas cher)','images/icons/vine-glass.png');
+        for (var i = 0; i < 50; i++) {
+            setMarker(map, new google.maps.LatLng(myMarkers[i].lat, myMarkers[i].lng), myMarkers[i].title, myMarkers[i].content, myMarkers[i].icon);
+        }
         
-
     };
     
     return {
---
+++ js/factory.js
@@ -0,0 +1,274 @@
+        var myMarkers=
+        [{
+                lat: 48.852179, 
+                lng: 2.335257, 
+                title:'Da Pietro', 
+                category : 'Italien',
+                content:'<b>Da Pietro </b>: Pizza simple',
+                icon: './images/icons/pizza2.png'
+        },{
+                lat: 48.850292, 
+                lng: 2.330776, 
+                title:'Pizza Chic', 
+                category : 'Italien',
+                content:'b>Pizza Chic </b>: resto sympa',
+                icon: './images/icons/pizza2.png'
+        },{
+                lat: 48.851535, 
+                lng: 2.338173, 
+                title:'Marco Polo', 
+                category : 'Italien',
+                content:'<b>Marco Polo </b>: Pâtes à la truffe',
+                icon:'./images/icons/pasta2.png'
+        },{
+                lat: 48.852886, 
+                lng: 2.331436, 
+                title:'Bocca della Verita', 
+                category : 'Italien',
+                content:'<b>Bocca della Verita</b> : Bonnes pâtes',
+                icon:'./images/icons/pasta2.png'
+        },{
+                lat: 48.842103, 
+                lng: 2.335142, 
+                title:'Coffee Club',
+                category: 'Americain',
+                content:'<b>Coffee Club</b> : américain classique',
+                icon:'./images/icons/hamburger.png'
+        },{
+                lat: 48.847043, 
+                lng: 2.325963, 
+                title:'Colorova', 
+                category: 'Brunch',
+                content:'<b>Colorova </b>: Pâtisserie, Brunch ou Gouter très sympa',
+                icon:'./images/icons/cookies.png'
+        },{
+                lat: 48.854928, 
+                lng: 2.341023,
+                title: 'Ze Kitchen Gallery', 
+                category: 'Francais Chic',
+                content: '<b>Ze Kitchen Gallery</b> : mediterranéen, 1 etoile Michelin',
+                icon: './images/icons/tuxedo.png'
+       },{ 
+                lat: 48.856560, 
+                lng: 2.326689,
+                title: 'Gaya', 
+                category: 'Francais Chic',
+                content: '<b>Gaya </b>: 1 etoile Michelin', 
+                icon: './images/icons/tuxedo.png'
+       },{ 
+                lat: 48.854450, 
+                lng: 2.337923,
+                title: 'Alcazar', 
+                category: 'Francais Chic',
+                content: '<b>Alcazar </b>: cool et assez bon', 
+                icon: './images/icons/tuxedo.png'
+       },{ 
+                lat: 48.854273,
+                lng: 2.330790,
+                title: 'Ralph', 
+                category: 'Americain',
+                content: '<b>Le Ralph\'s</b>: américain chic', 
+                icon: './images/icons/hamburger.png'
+       },{      
+                lat: 48.847343, 
+                lng: 2.318231,
+                title: 'Patisserie Cyril Lignac', 
+                category: 'Goûter',
+                content: '<b>Patisserie Cyril Lignac </b>: à emporter',
+                icon: './images/icons/cookies.png'
+       },{ 
+                lat: 48.853048, 
+                lng: 2.338647,
+                title: 'Eclair de Genie', 
+                category: 'Goûter',
+                content: '<b>Eclair de Genie</b>: à emporter', 
+                icon: './images/icons/cookies.png'
+       },{
+                lat: 48.852098, 
+                lng: 2.346457,
+                title: 'Odette',
+                category: 'Goûter',
+                content: '<b>Odette</b> : choux à la crème',
+                icon: './images/icons/dounoughts.png'
+       },{
+                lat: 48.849555, 
+                lng: 2.349307,
+                title: 'Aux Merveilleux de Fred',
+                category: 'Goûter',
+                content: '<b>Aux Merveilleux de Fred</b> : classique',
+                icon: './images/icons/dounoughts.png'
+       },{
+                lat: 48.840031, 
+                lng: 2.335252,
+                title: 'Il Forno di Napoli',
+                category: 'Italien',
+                content: '<b>Il Forno di Napoli</b> : très bonne pizza, à emporter',
+                icon: './images/icons/pizza2.png'
+       },{ 
+                lat: 48.852671, 
+                lng: 2.331512,
+                title: 'Blueberry',
+                category: 'Japonais',
+                content: '<b>Blueberry </b>: maki bar',
+                icon: './images/icons/sushi-2.png'
+       },{ 
+                lat: 48.853151, 
+                lng: 2.337829,
+                title: 'Ippudo',
+                category:'Japonais',
+                content: '<b>Ippudo</b> : spécialiste du ramen',
+                icon: './images/icons/sushi-3.png'
+       },{ 
+                lat: 48.852228,
+                lng: 2.329959,
+                title: 'Bar de la Croix Rouge',
+                category: 'Terrasse',
+                content: '<b>Bar de la Croix Rouge </b>: Toasts, Terrasse',
+                icon: './images/icons/sun.png'
+       },{ 
+                lat: 48.858175,
+                lng: 2.356220,
+                title: 'Benedict',
+                category: 'Brunch',
+                content: '<b>Benedict </b>: brunch, souvent la queue, oeufs pochés',
+                icon: './images/icons/egg.png'
+       },{
+                lat: 48.857122, 
+                lng: 2.358887,
+                title: 'Miznon',
+                category: 'Cacher',
+                content: '<b>Miznon </b>: cacher, israelien, fermé samedi',
+                icon: './images/icons/meat.png'
+       },{
+                lat: 48.854234, 
+                lng: 2.350212,
+                title: 'Bertie’s CupCakery',
+                category: 'Goûter',
+                content: '<b>Bertie’s CupCakery  </b>: meilleur cupcake Paris',
+                icon: './images/icons/dounoughts.png'
+       },{
+                lat: 48.862378, 
+                lng: 2.363778,
+                title: 'Popelini',
+                category: 'Goûter',
+                content: '<b>Popelini </b>: choux à la crème',
+                icon: './images/icons/cookies.png'
+       },{
+                lat: 48.842632, 
+                lng: 2.328002,
+                title: 'Café Atelier',
+                category: 'Brunch',
+                content: '<b>Café Atelier </b>: bistro, brunch, bon',
+                icon: './images/icons/brunch.png' 
+       },{
+                lat: 48.844593, 
+                lng: 2.331951,
+                title: 'Jean Paul Hévin',
+                category: 'Goûter',
+                content: '<b>Jean Paul Hévin</b> : Chocolatier, Macaron, Cheesecake',
+                icon: './images/icons/cookies.png'
+       },{
+                lat: 48.853903,
+                lng: 2.332277,
+                title: 'Café Pouchkine',
+                category: 'Goûter',
+                content: '<b>Café Pouchkine </b> : salon de thé franco-russe',
+                icon: './images/icons/cookies.png'
+       },{
+                lat: 48.848880,lng: 2.334119,
+                title: 'Angelina',
+                category: 'Goûter',
+                content: '<b>Angelina</b> : peu de couvert, dans le Luxembourg',
+                icon: './images/icons/tea.png'
+       },{
+                lat: 48.844716,
+                lng: 2.332012,
+                title: 'Amorino',
+                category: 'Goûter',
+                content: '<b>Amorino </b>: glaces',
+                icon: './images/icons/ice-cream.png'
+       },{
+                lat: 48.854374,
+                lng: 2.333503,
+                title: 'La Société',
+                category: 'Francais Chic',
+                content: '<b>La Société</b> : costes, belle terrasse, cher',
+                icon: './images/icons/tuxedo.png'
+       },{
+                lat: 48.865317,
+                lng: 2.331279,
+                title: 'La Corte',
+                category : 'Italien',
+                content: '<b>La Corte</b>: Italien, très bon, à reserver',
+                icon: './images/icons/pasta2.png'
+        },{
+                lat: 48.888272,
+                lng: 2.362532,
+                title: 'Bob’s Bake Shop',
+                category: 'Goûter',
+                content: '<b>Bob’s Bake Shop </b>: tartes, bagels, toast, terrasse',
+                icon: './images/icons/hot-dog.png'
+       },{ 
+                lat: 48.867657,
+                lng: 2.331341,
+                title: 'Little Georgette',
+                category: 'Bistrot',
+                content: '<b>Little Georgette</b>: tapas francais',
+                icon: './images/icons/hot-dog.png'
+       },{
+                lat: 48.866089,
+                lng: 2.345371,
+                title: 'La Cevicheria',
+                category: 'Asiatique',
+                content: '<b>La Cevicheria</b>: spécialiste des ceviche',
+                icon: './images/icons/sushi.png'
+       },{
+                lat: 48.872299,
+                lng: 2.365012,
+                title: 'Siseng',
+                category: 'Asiatique',
+                content: '<b>Siseng </b>: hamburger blanc, asiatique',
+                icon: './images/icons/hamburger2.png'
+       },{
+                lat: 48.864677,
+                lng: 2.333851,
+                title: 'Sanukiya',
+                category:'Japonais',
+                content: '<b>Sanukiya</b>: spécialiste du Udon',
+                icon: './images/icons/sushi.png'
+       },{
+                lat: 48.877078,
+                lng: 2.340834,
+                title: 'Caillebotte',
+                category: 'Bistrot',
+                content: '<b>Caillebotte</b>: Bistrot branché, beau décor',
+                icon: './images/icons/vine-glass.png'
+       },{
+                lat: 48.879298,
+                lng: 2.334576,
+                title: 'Les Canailles',
+                category: 'Bistrot',
+                content: '<b>Les Canailles</b>: Bistrot gastro, pas très cher',
+                icon: './images/icons/vine-glass.png'
+       },{
+                lat: 48.858413,
+                lng: 2.302294,
+                title: 'Fables de la Fontaine',
+                category: 'Francais Chic',
+                content: '<b>Fables de la Fontaine</b>: restaurant gastro, pas cher',
+                icon: './images/icons/vine-glass.png'
+       },{
+                lat: 48.851887, 
+                lng: 2.344328,
+                title: 'Lengue',
+                category:'Japonais',
+                content: '<b>Lengue</b>: vrai japonais',
+                icon: './images/icons/sushi.png'
+       },{
+                lat: 48.849684, 
+                lng: 2.354449,
+                title: 'Restaurant A.T',
+                category:'Japonais',
+                content: '<b>Restaurant A.T by </b>: japonais, fusion food, chic',
+                icon: './images/icons/tuxedo.png'
+       }] 
